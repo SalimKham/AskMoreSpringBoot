90b0001eaecbc5385adbb79e1ac9b5cb8c7054e4
Khamadj Nasreddine <salim.kham1991@gmail.COM>
2019-05-26 12:37:22 +0300

    Update FileController.java

--- src/main/java/io/khaminfo/ppmtool/web/FileController.java
+++ src/main/java/io/khaminfo/ppmtool/web/FileController.java
@@ -50,7 +50,7 @@ public class FileController {
 	@GetMapping("/profile/{fileName}")
 	public void getImage(@PathVariable String  fileName ,  HttpServletResponse response) throws IOException {
 		System.out.println("We are here" + fileName);
-		 Path path = Paths.get("src/main/resources/static"+"/profile-picture/" + fileName);
+		 Path path = Paths.get("src/main/resources/static"+"/profile_picture/" + fileName);
 		 DataInputStream in = new DataInputStream(new FileInputStream(path.toFile().getAbsolutePath()));
 		response.setHeader("Content-disposition: ", "attachment; filename="+fileName);
 		response.setContentType("image/png");
